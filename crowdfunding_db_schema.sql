af70ed79027bdbfe99f37e3d8884f62bce9ebf1d
rodneaa <136031276+rodneaa@users.noreply.github.com>
2023-09-15 00:05:10 -0400

    Corrected File with Select Statements

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -3,15 +3,15 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "Category" (
-    "category_id" varchar   NOT NULL,
-    "category" varchar   NOT NULL,
-    CONSTRAINT "pk_Category" PRIMARY KEY (
-        "category_id"
-     )
+CREATE TABLE crowdfunding_db_schema.Category (
+    category_id varchar   NOT NULL PRIMARY KEY,
+    category varchar  NOT NULL 
 );
 
-CREATE TABLE "Subcategory" (
+Select * from crowdfunding_db_schema.category;
+
+-------------------------------------------------------------------------------
+CREATE TABLE crowdfunding_db_schema.Subcategory (
     "subcategory_id" varchar   NOT NULL,
     "subcategory_name" varchar   NOT NULL,
     CONSTRAINT "pk_Subcategory" PRIMARY KEY (
@@ -19,19 +19,23 @@ CREATE TABLE "Subcategory" (
      )
 );
 
-CREATE TABLE "Contacts" (
-    "contact_id" int   NOT NULL,
-    "first_name" varchar   NOT NULL,
-    "last_name" varchar   NOT NULL,
-    "email" varchar   NOT NULL,
-    CONSTRAINT "pk_Contacts" PRIMARY KEY (
-        "contact_id"
-     )
+Select * from crowdfunding_db_schema.Subcategory;
+
+-------------------------------------------------------------------------------
+CREATE TABLE crowdfunding_db_schema.Contacts (
+   contact_id SERIAL  NOT NULL  PRIMARY KEY,
+    first_name varchar   NOT NULL,
+    last_name varchar   NOT NULL,
+    email varchar   NOT NULL 
 );
 
-CREATE TABLE "Campaign" (
-    "cf_id" int   NOT NULL,
-    "contact_id" int   NOT NULL,
+Select * from crowdfunding_db_schema.Contacts;
+
+-------------------------------------------------------------------------------
+CREATE TABLE crowdfunding_db_schema.Campaign (
+    cf_id int   NOT NULL PRIMARY KEY,
+    contact_id SERIAL   NOT NULL,
+	FOREIGN KEY (contact_id) REFERENCES crowdfunding_db_schema.Contacts(contact_id),
     "company_name" varchar   NOT NULL,
     "description" varchar   NOT NULL,
     "goal" float   NOT NULL,
@@ -42,19 +46,26 @@ CREATE TABLE "Campaign" (
     "currency" varchar   NOT NULL,
     "launched_date" date   NOT NULL,
     "end_date" date   NOT NULL,
-    "category_id" varchar   NOT NULL,
-    "subcategory_id" varchar   NOT NULL,
-    CONSTRAINT "pk_Campaign" PRIMARY KEY (
-        "cf_id"
-     )
+    category_id varchar   NOT NULL,
+	FOREIGN KEY (category_id) REFERENCES crowdfunding_db_schema.Category(category_id),
+    subcategory_id varchar   NOT NULL,
+	FOREIGN KEY (subcategory_id) REFERENCES crowdfunding_db_schema.Subcategory(subcategory_id)
 );
 
-ALTER TABLE "Category" ADD CONSTRAINT "fk_Category_category_id" FOREIGN KEY("category_id")
-REFERENCES "Campaign" ("category_id");
+SELECT * FROM crowdfunding_db_schema.Campaign;
 
-ALTER TABLE "Subcategory" ADD CONSTRAINT "fk_Subcategory_subcategory_id" FOREIGN KEY("subcategory_id")
-REFERENCES "Campaign" ("subcategory_id");
 
-ALTER TABLE "Contacts" ADD CONSTRAINT "fk_Contacts_contact_id" FOREIGN KEY("contact_id")
-REFERENCES "Campaign" ("contact_id");
+
+
+
+
+
+
+
+
+
+
+
+
+
 
